28b85484e1a80d1457b8573d1ef275eb9ac466cb
Derek <teodorico12@gmail.com>
2016-08-22 20:50:44 -0700

    started work on the panel for Signal, Attention and Meditation values

---
+++ src/main/java/com/DerekCo/eeg/NeuroSkyPanel.java
@@ -0,0 +1,17 @@
+package com.DerekCo.eeg;
+
+import javax.swing.*;
+
+/**
+ * Created by Mastermind on 8/22/16.
+ * houses the charts for Signal Strength, Attention and Meditation.
+ */
+public class NeuroSkyPanel {
+
+    JPanel panel;
+
+    public NeuroSkyPanel(){
+        panel = new JPanel();
+
+    }
+}
