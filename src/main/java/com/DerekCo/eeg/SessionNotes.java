a2fcf81b3c6ba932bfcacd896b520ba7f9b3da26
Derek <teodorico12@gmail.com>
2016-08-31 13:37:58 -0700

    added functionality to session notes fields, created dir for notes to end up

--- src/main/java/com/DerekCo/eeg/SessionNotes.java
+++ src/main/java/com/DerekCo/eeg/SessionNotes.java
@@ -2,6 +2,10 @@ package com.DerekCo.eeg;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by Mastermind on 8/25/16.
@@ -11,8 +15,8 @@ public class SessionNotes {
     JPanel buffer;
     JTextArea recordField;
     JTextArea inputField;
+    JTextField inputLabel;
     JButton submit;
-    JButton mark;
 
     public SessionNotes(){
         panel = new JPanel();
@@ -21,11 +25,16 @@ public class SessionNotes {
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         panel.setPreferredSize(new Dimension(260, 350));
 
-        recordField = new JTextArea(3, 4);
+        recordField = new JTextArea(3, 10);
         recordField.setEditable(false);
         recordField.setLineWrap(true);
 
         buffer = new JPanel();
+        buffer.setBackground(Color.LIGHT_GRAY);
+        inputLabel = new JTextField("Add Note:");
+        inputLabel.setEditable(false);
+        inputLabel.setBackground(Color.LIGHT_GRAY);
+        buffer.add(inputLabel);
 
         inputField = new JTextArea(3, 2);
         inputField.setEditable(true);
@@ -33,17 +42,30 @@ public class SessionNotes {
 
 
         submit = new JButton("Submit");
-        mark = new JButton("Mark");
+        submit.addActionListener(new SubmitButtonListener());
 
         panel.add(recordField);
         panel.add(buffer);
         panel.add(inputField);
         panel.add(submit);
-        panel.add(mark);
 
     }
 
     public JPanel getMainPanel() {
         return this.panel;
+    }
+
+    public String timeStamp() {
+        return new SimpleDateFormat("HH:mm:ss - ").format(new Date());
+    }
+
+    class SubmitButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            String noteText = inputField.getText() + "\n";
+            recordField.append(timeStamp());
+            recordField.append(noteText);
+            inputField.setText("");
+
+        }
     }
 }
