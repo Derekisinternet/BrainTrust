ce7956adb1bcd256e30554406a3b3ca17d1df740
Derek <teodorico12@gmail.com>
2016-09-04 16:45:41 -0700

    made session notes retrievable when window closed and re-opened

--- src/main/java/com/DerekCo/eeg/SessionNotes.java
+++ src/main/java/com/DerekCo/eeg/SessionNotes.java
@@ -1,7 +1,9 @@
 package com.DerekCo.eeg;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 
 /**
  * Created by Mastermind on 9/2/16.
@@ -21,6 +23,15 @@ public class SessionNotes {
 
     public String getNotes() {
         String notesRecord = "";
+        File file = new File(fileDir + fileName);
+        if (file.exists()) {
+            try {
+                String content = new Scanner(file).useDelimiter("\\Z").next();
+                return content;
+            } catch (FileNotFoundException exception) {
+                //maybe do something
+            }
+        }
         //if the file exists, return the contents
         //else, return an empty string.
         return notesRecord;
